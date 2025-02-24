3621725cd04879cfad6ff8fc838bfb5e882eff7b
Valentin Shinkarev <valent.shinkarev@gmail.com>
2025-02-25 01:49:32 +0300

    fix multiple results in git diff

--- nvim/lua/plugins/specs/diff.lua
+++ nvim/lua/plugins/specs/diff.lua
@@ -19,15 +19,15 @@ return {
     vim.keymap.set('n', '<leader>hr', reset_hunk, { expr = true, remap = true, desc = '[H]unk [R]eset' })
     vim.keymap.set('n', '<leader>ht', MiniDiff.toggle_overlay, { remap = true, desc = '[H]unk [T]oggle' })
 
-    local set_diff = function()
+    local set_diff = function(args)
       if vim.b.diff == nil then
         local content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
         MiniDiff.set_ref_text(0, content)
         vim.b.diff = true
       end
 
-      local ref = vim.g.commit or 'HEAD'
-      local path = vim.system({ 'git', 'ls-files', '--full-name', vim.fn.expand('%') }):wait().stdout:gsub("\n", "")
+      local ref = vim.g.commit or ''
+      local path = vim.system({ 'git', 'ls-files', '--full-name', vim.fn.expand('%') }):wait().stdout:gsub('\n', '')
       if path == "" then
         return
       end
@@ -35,18 +35,42 @@ return {
       local content = vim.system({ 'git', 'show', ref .. ':' .. path }):wait().stdout
       MiniDiff.set_ref_text(0, content)
 
-      vim.api.nvim_create_user_command('Diff', function()
-        vim.cmd [[
-          cexpr system("git diff -U1 --compact-summary --dst-prefix=$(echo $(git rev-parse --show-toplevel)'/') | grep -oE '^[+][+][+].*|^@@[^@]*' | sed 's/@@ [-,0-9]* +\\([0-9]*\\)/:\\1:1/g' | sed '/^+++/{N;s/+++ \\(.*\\)\\n\\(.*\\)/\\1\\2/;}'") | copen
-        ]]
-      end, {})
+      vim.api.nvim_create_user_command('Diff', function(args)
+        local cmd = 'git diff -U0 --dst-prefix=$(git rev-parse --show-toplevel)/'
+        args = args.args
+        if args == '' then
+          args = ref
+        end
+        local diff = vim.system({ 'sh', '-c', cmd .. ' ' .. args }):wait().stdout
+        local entries = {}
+
+        local file
+        for value in diff:gmatch('[^\n]+') do
+          if value:match('^+++') then
+            file = value:gsub('^+++ ', '')
+          elseif value:match('^@@') then
+            local i, j = value:find('+[0-9]+')
+            local lnum = value:sub(i, j):gsub('+', '')
+            local blame = vim.system({ 'git', 'blame', file, '-L', lnum .. ',' .. lnum }):wait().stdout
+            i, j = blame:find('%([^)]+%)')
+            entries[#entries + 1] = { filename = file, lnum = lnum, text = blame:sub(i, j) }
+          end
+        end
+
+        vim.fn.setqflist(entries)
+        vim.cmd.copen()
+      end, { nargs = '*' })
     end
 
-    vim.api.nvim_create_autocmd('BufEnter', {
+    vim.api.nvim_create_user_command('SetDiff', function(args)
+      vim.g.commit = args.args
+      set_diff()
+    end, { nargs = '*' })
+
+
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWrite' }, {
       desc = 'SetDiffSource',
       callback = set_diff,
     })
-
-    vim.api.nvim_create_user_command('DiffReload', set_diff, {})
   end
 }
