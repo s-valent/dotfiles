b56689ef5c35e86b96fac66c614b4ff118d012b3
Valentin Shinkarev <valent.shinkarev@gmail.com>
2025-03-02 00:09:24 +0300

    some adjustments

--- nvim/lua/plugins/specs/treesitter.lua
+++ nvim/lua/plugins/specs/treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ':TSUpdate',
     opts = {
       auto_install = true,
-      indent = { enable = true },
+      -- indent = { enable = true },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
--- nvim/queries/python/highlights.scm
+++ nvim/queries/python/highlights.scm
@@ -3,7 +3,7 @@
 ((identifier) @variable (#match? @variable "^[A-Z]"))
 (class_definition name: (identifier) @type)
 
-((identifier) @constant (#match? @constant "^[A-Z][A-Z0-9]+$"))
+((identifier) @constant (#match? @constant "^[A-Z_][A-Z0-9_]+$"))
 
 
 ;; copied from https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/python/highlights.scm
